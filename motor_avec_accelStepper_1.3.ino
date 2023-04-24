d31b4eac0eee72716b24626897e8d74029a3ae40
arnaud leduc <arnaud.leduc@keyconsulting.fr>
2023-04-24 15:26:54 +0200

    fix date change via serial input

--- motor_avec_accelStepper_1.3.ino
+++ motor_avec_accelStepper_1.3.ino
@@ -1,15 +1,27 @@
 #include <DS3231.h> //https://github.com/jarzebski
-
+#include <LiquidCrystal.h>
 #include <AccelStepper.h>
 #define HALFSTEP 8  //Half-step mode (8 step control signal sequence)
 
 #include <Wire.h>
-
+// affichage
+LiquidCrystal lcd(13, 12, 7, 6, 5, 4);
+//// affichage caratère spéciaux
+byte degre[8] = {
+  0b00000,
+  0b01000,
+  0b00100,
+  0b11110,
+  0b11111,
+  0b11110,
+  0b00100,
+  0b01000,
+};
 //horloge
 RTCDateTime dt;
 boolean isAlarm = false;
 boolean alarmState = false;
-byte Year ;
+uint16_t Year ;
 byte Month ;
 byte Date ;
 byte DoW ;
@@ -50,32 +62,36 @@ int Seconde2 = 00;
 int currentAlarm = 1;
 
 void calibrate() {
-  Serial.println("début calibrage position moteur"); 
+  lcd.setCursor(0, 0);
+  lcd.print("Calibrage");
   opticalSwitch = analogRead(A0);
   stepper.setSpeed(speed);
-  while(opticalSwitch > 110 || digitalRead(triggerCalibragePin) == LOW)
+  while (opticalSwitch > 110 || digitalRead(triggerCalibragePin) == LOW)
   {
     stepper.runSpeed();
     opticalSwitch = analogRead(A0);
   }
   stepper.setCurrentPosition(0);
-  Serial.println("fin calibrage position moteur"); 
-  digitalWrite(mtrPin1,LOW);
-  digitalWrite(mtrPin2,LOW);     
-  digitalWrite(mtrPin3,LOW);   
-  digitalWrite(mtrPin4,LOW); 
+  digitalWrite(mtrPin1, LOW);
+  digitalWrite(mtrPin2, LOW);
+  digitalWrite(mtrPin3, LOW);
+  digitalWrite(mtrPin4, LOW);
+  lcd.clear();
   return;
 }
 
 void distribute() {
+  lcd.setCursor(0, 0);
+  lcd.print("Distribution");
+  lcd.setCursor(0, 1);
+  lcd.print("en cours");
   stepper.setCurrentPosition(0);
-  Serial.println("début versement croquettes"); 
   stepper.setSpeed(speed);
-  while(stepper.currentPosition() != (angleMini*nbPortion)) // positionnement 
+  while (stepper.currentPosition() != (angleMini * nbPortion)) // positionnement
   {
     stepper.runSpeed();
   }
-
+  lcd.clear();
   return;
 }
 
@@ -87,11 +103,15 @@ void alarmFunction()
 }
 
 void setup() {
-  
+  lcd.begin(16, 2);
+  lcd.createChar(0, degre);
+
   pinMode(triggerCalibragePin, INPUT_PULLUP);
   Serial.begin(9600);
-  Serial.println("------------------------------------");
-  Serial.println("------  Croquette distributor   ----");
+  lcd.setCursor(0, 0);
+  lcd.print("Distri Croquette");
+  delay(2000);
+  lcd.clear();
   stepper.setMaxSpeed(2000.0);
   stepper.setAcceleration(2000.0);  //Make the acc quick
   stepper.setSpeed(2000);
@@ -102,22 +122,23 @@ void setup() {
   clock.clearAlarm1();
   clock.clearAlarm2();
   clock.setAlarm1(0, Heure1, Minute1, Seconde1, DS3231_MATCH_H_M_S);
-//  clock.setAlarm1(0, 8, 30, 0, DS3231_MATCH_H_M_S);
+  //  clock.setAlarm1(0, 8, 30, 0, DS3231_MATCH_H_M_S);
   //clock.setAlarm2(0, 0, 45, DS3231_MATCH_M);
-//  clock.setAlarm2(0, 18, 30, DS3231_MATCH_H_M);
+  //  clock.setAlarm2(0, 18, 30, DS3231_MATCH_H_M);
   attachInterrupt(0, alarmFunction, FALLING);
- 
+
 }
 
 void loop() {
   RTCAlarmTime alarm;
   setDate();
-  readRTC();
+  dt = clock.getDateTime();
+  Serial.print("heure : "); Serial.println(clock.dateFormat("d-m-Y H:i:s", dt));
   // on regarde si il y a un clibrage
-  if(digitalRead(triggerCalibragePin) == LOW){
+  if (digitalRead(triggerCalibragePin) == LOW) {
     calibrate();
   }
-  
+
   if (isAlarm) {
     Serial.println("Alarm detectee");
     isAlarm = false;
@@ -130,84 +151,90 @@ void loop() {
       currentAlarm = 1;
       clock.setAlarm1(0, Heure1, Minute1, Seconde1, DS3231_MATCH_H_M_S);
     }
-    
+
     //stop the motor
-    digitalWrite(mtrPin1,LOW);
-    digitalWrite(mtrPin2,LOW);     
-    digitalWrite(mtrPin3,LOW);   
-    digitalWrite(mtrPin4,LOW); 
-    Serial.print("distibution à : ");Serial.println(clock.dateFormat("d-m-Y H:i:s", dt)); 
-  } 
-  alarm=clock.getAlarm1();
-  Serial.print("prochaine distibution à : ");Serial.println(clock.dateFormat("H:i:s", alarm)); 
-  delay(250); 
-}
+    digitalWrite(mtrPin1, LOW);
+    digitalWrite(mtrPin2, LOW);
+    digitalWrite(mtrPin3, LOW);
+    digitalWrite(mtrPin4, LOW);
+    Serial.print("distibution : "); Serial.println(clock.dateFormat("d-m-Y H:i:s", dt));
+  }
+  alarm = clock.getAlarm1();
+  Serial.print("distibution à: "); Serial.println(clock.dateFormat("H:i:s", alarm));
+  lcd.setCursor(0, 0);
+  lcd.print(clock.dateFormat("d-m-Y H:i:s", dt));
+  lcd.setCursor(6, 1);
+  lcd.write((byte)0);
+  lcd.setCursor(8, 1);
+  lcd.print(clock.dateFormat("H:i:s", alarm));
 
-void readRTC( ) { /* function readRTC */
- ////Read Real Time clock
-  dt = clock.getDateTime();
-  Serial.print("heure : ");Serial.println(clock.dateFormat("d-m-Y H:i:s", dt));
+  delay(250);
 }
-
 
 void setDate( ) { /* function setDate */
- ////Set Real Time clock
- if (Serial.available()) {
-   //int _start = millis();
-   GetDateStuff(Year, Month, Date, DoW, Hour, Minute, Second);
+  ////Set Real Time clock
+  if (Serial.available()) {
+    //int _start = millis();
+    GetDateStuff(Year, Month, Date, DoW, Hour, Minute, Second);
     clock.setDateTime(Year, Month, Date, Hour, Minute, Second);
+    delay(250);
 
- }
+  }
 }
-void GetDateStuff(byte& Year, byte& Month, byte& Day, byte& DoW, byte& Hour, byte& Minute, byte& Second) { /* function GetDateStuff */
- ////Get date data
- // Call this if you notice something coming in on
- // the serial port. The stuff coming in should be in
- // the order YYYYMMDDwHHMMSS, with an 'x' at the end. Ex. 20220714w081400x
- boolean GotString = false;
- char InChar;
- byte Temp1, Temp2, Temp3, Temp4;
- char InString[20];
- byte j = 0;
- while (!GotString) {
-   if (Serial.available()) {
-     InChar = Serial.read();
-     InString[j] = InChar;
-     j += 1;
-     if (InChar == 'x') {
-       GotString = true;
-     }
-   }
- }
- Serial.println(InString);
- // Read Year first
- Temp1 = (byte)InString[0] - 48;
- Temp2 = (byte)InString[1] - 48;
- Temp3 = (byte)InString[2] - 48;
- Temp4 = (byte)InString[3] - 48;
- Year = Temp1 * 1000 + Temp2 * 100 + Temp3 * 10 + Temp4;
+void GetDateStuff(uint16_t& Year, byte& Month, byte& Day, byte& DoW, byte& Hour, byte& Minute, byte& Second) { /* function GetDateStuff */
+  ////Get date data
+  // Call this if you notice something coming in on
+  // the serial port. The stuff coming in should be in
+  // the order YYYYMMDDwHHMMSS, with an 'x' at the end. Ex. 20220714w081400x
+  boolean GotString = false;
+  char InChar;
+  byte Temp1, Temp2, Temp3, Temp4;
+  char InString[20];
+  byte j = 0; Serial.println("test 5 ");
+  while (!GotString) {
+    if (Serial.available() > 0) {
+      InChar = Serial.read();
+      InString[j] = InChar;
+      j += 1;
+      if (InChar == 'x') {
+        GotString = true;
+        while (Serial.available() > 0) Serial.read(); // remove all remaining char
+      }
+    }
+  }
+  Serial.println(InString);
+  // Read Year first
+  Temp1 = (byte)InString[0] - 48;
+  Serial.println(Temp1 * 1000);
+  Temp2 = (byte)InString[1] - 48;
+  Serial.println(Temp2 * 100);
+  Temp3 = (byte)InString[2] - 48;
+  Serial.println(Temp3 * 10);
+  Temp4 = (byte)InString[3] - 48;
+  Serial.println(Temp4);
+  Year = Temp1 * 1000 + Temp2 * 100 + Temp3 * 10 + Temp4;
   Serial.print("Year ");
   Serial.println(Year);
- // now month
- Temp1 = (byte)InString[4] - 48;
- Temp2 = (byte)InString[5] - 48;
- Month = Temp1 * 10 + Temp2;
- // now date
- Temp1 = (byte)InString[6] - 48;
- Temp2 = (byte)InString[7] - 48;
- Day = Temp1 * 10 + Temp2;
- // now Day of Week
- DoW = (byte)InString[8] - 48;
- // now Hour
- Temp1 = (byte)InString[9] - 48;
- Temp2 = (byte)InString[10] - 48;
- Hour = Temp1 * 10 + Temp2;
- // now Minute
- Temp1 = (byte)InString[11] - 48;
- Temp2 = (byte)InString[12] - 48;
- Minute = Temp1 * 10 + Temp2;
- // now Second
- Temp1 = (byte)InString[13] - 48;
- Temp2 = (byte)InString[14] - 48;
- Second = Temp1 * 10 + Temp2;
+  // now month
+  Temp1 = (byte)InString[4] - 48;
+  Temp2 = (byte)InString[5] - 48;
+  Month = Temp1 * 10 + Temp2;
+  // now date
+  Temp1 = (byte)InString[6] - 48;
+  Temp2 = (byte)InString[7] - 48;
+  Day = Temp1 * 10 + Temp2;
+  // now Day of Week
+  DoW = (byte)InString[8] - 48;
+  // now Hour
+  Temp1 = (byte)InString[9] - 48;
+  Temp2 = (byte)InString[10] - 48;
+  Hour = Temp1 * 10 + Temp2;
+  // now Minute
+  Temp1 = (byte)InString[11] - 48;
+  Temp2 = (byte)InString[12] - 48;
+  Minute = Temp1 * 10 + Temp2;
+  // now Second
+  Temp1 = (byte)InString[13] - 48;
+  Temp2 = (byte)InString[14] - 48;
+  Second = Temp1 * 10 + Temp2;
 }
